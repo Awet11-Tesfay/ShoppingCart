4e0e718ac268106d19fd25a96880ffeac0db4256
BirukL137 <buraf13k@gmail.com>
2023-06-11 00:18:39 +0300

    Add signup url to urlpatterns

--- ecommerce/authapp/urls.py
+++ ecommerce/authapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from authapp import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
+    path('signup/', views.signup, name="signup"),
 ]
