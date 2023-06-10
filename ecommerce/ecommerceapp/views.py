fb93781f7e7e395649e98529e645def55282fd79
Awet11-Tesfay <hailegb2021@gmail.com>
2023-06-11 01:38:10 +0300

    contact_form_function

--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from ecommerceapp.models import Product
+from ecommerceapp.models import Product, Contact
 from django.contrib import messages
 from math import ceil
 from django.conf import settings
@@ -25,4 +25,15 @@ def index(request):
     return render(request, "index.html")
 
 
-
+def contact(request):
+    if request.method=='POST':
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        desc=request.POST.get("desc")
+        pnumber=request.POST.get("pnumber")
+        myquery=Contact(name=name,email=email,desc=desc,phonenumber=pnumber)
+        myquery.save()
+        messages.info(request,"We will get back to you soon...")
+        return render(request,"contact.html")
+    
+    return render(request,"contact.html")
