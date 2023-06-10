33f12b1aa30d60e7ee87faeb2f47a48aa74fb8ec
BirukL137 <buraf13k@gmail.com>
2023-06-11 00:32:39 +0300

    Add login view function to views.py

--- ecommerce/authapp/views.py
+++ ecommerce/authapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth import authenticate
 
 
 # Create your views here.
@@ -21,4 +22,20 @@ def signup(request):
             return redirect('/auth/login')
 
     return render(request,"signup.html")
- 
+
+
+def login(request):
+    if request.method=="POST":
+        username=request.POST['email']
+        userpassword=request.POST['pass1']
+        myuser=authenticate(username=username,password=userpassword)
+
+        if myuser is not None:
+            auth.login(request,myuser)
+            messages.success(request,"Login Success")
+            return redirect('/')
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('/auth/login')
+
+    return render(request, 'login.html')
