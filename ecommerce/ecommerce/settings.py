c6cd245494da8b377a6f76f629b666f3c288a1ae
Awet11-Tesfay <hailegb2021@gmail.com>
2023-06-11 01:17:26 +0300

    setting_files

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -120,13 +120,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_URLS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIAFILES_URLS = [
-    os.path.join(BASE_DIR, 'media')
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
