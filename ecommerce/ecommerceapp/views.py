acf542f3d7802ff09e80a76051bef16ecbfeb062
Awet11-Tesfay <hailegb2021@gmail.com>
2023-06-11 01:08:07 +0300

    'home_function'

--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render,redirect
+from ecommerceapp.models import Product
+from django.contrib import messages
+from math import ceil
+from ecommerceapp import keys
+from django.conf import settings
+import json
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    print(catprods)
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4 ) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    params = {'allProds':allProds}
+
+    return render(request,"index.html",params)
+
+
+    return render(request, "index.html")
