34bf2bfd457ea477569fb8e3ed63192f5dc86e6d
Awet11-Tesfay <hailegb2021@gmail.com>
2023-06-14 10:51:03 +0300

    'adding_order_orderupdate'

--- ecommerce/ecommerceapp/admin.py
+++ ecommerce/ecommerceapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from ecommerceapp.models import Product, Contact, Orders
+from ecommerceapp.models import Product, Contact, Orders, OrderUpdate
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Contact)
 admin.site.register(Orders)
+admin.site.register(OrderUpdate)
